014e7a65540a5151a641c326cee7c24ad5fdfdf7
jacobgoskha <sardaukarkk@gmail.com>
2017-10-08 09:12:12 -0400

    reverse String challenge

---
+++ 1-reverseAString.js
@@ -0,0 +1,14 @@
+var reversed ='';
+
+function reverseString(str) {
+    var reversed ='';
+    for (var i = str.length -1; i >= 0 ;i--){
+        reversed += str[i];
+        }
+        str = reversed;
+return str;
+}
+ 
+console.log(reverseString("hello world"));
+
+
