3845f1723949b5bcb7fb752a1f9cc40500cacd7a
jacobgoskha <sardaukarkk@gmail.com>
2017-10-15 07:40:21 -0400

    backgorund color changed

---
+++ 14_seekAndDestroy.js
@@ -0,0 +1,12 @@
+function destroyer(arr) {
+    // Remove all the values
+    return arr;
+  }
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+  var x = [[1, 2, 3, 1, 2, 3], 2, 3];
+  console.log(x[0]);
+  console.log(x[1]);
+  console.log(x[2]);
